be0e279baa7669e4fa7c8592bfe64bf27fe61a2f
Kirsten Buchan <kirsten2412@hotmail.co.uk>
2021-12-04 11:16:44 +0000

    add animal form

--- controllers/animals_controller.py
+++ controllers/animals_controller.py
@@ -1,6 +1,37 @@
 from flask import Flask, render_template, request,redirect,url_for, Blueprint
 import repositories.animal_repository as animal_repository
+import repositories.owner_repository as owner_repository
+import repositories.vet_repository as vet_repository
 from models.animal import Animal
 
 animals_blueprint = Blueprint("animals", __name__)
 
+# INDEX
+# GET '/animals'
+@animals_blueprint.route("/animals")
+def animals():
+    animals = animal_repository.select_all()
+    return render_template("animals/index.html", animals=animals)
+
+# NEW
+# GET '/animals/new'
+# Return html form to the browser
+@animals_blueprint.route("/animals/new")
+def new_animal():
+    return render_template("animals/new.html")
+
+# CREATE
+# POST '/animals'
+@animals_blueprint.route("/animals", methods=['POST'])
+def create_animal():
+    name = request.form['name']
+    date_of_birth = request.form['date_of_birth']
+    type_of_animal = request.form['type_of_animal']
+    owner_id = request.form['owner_id']
+    treatment_notes = request.form['treatment_notes']
+    vet_id = request.form['vet_id']
+    owner = owner_repository.select(owner_id)
+    vet = vet_repository.select(vet_id)
+    animal = Animal(name, date_of_birth,type_of_animal, owner, treatment_notes, vet)
+    animal_repository.save(animal)
+    return redirect(url_for(".animals"))
